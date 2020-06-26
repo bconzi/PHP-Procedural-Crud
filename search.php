ac151a12e3269c5c837314492af79dbe6e4fd404
Brenda Conzi <brendayconzi@gmail.com>
2020-06-26 20:10:32 +0200

    Correction with the redirection

--- search.php
+++ search.php
@@ -2,85 +2,148 @@
 <html>
     <head>
         <title>Cellphones List</title>
-          <link rel="stylesheet" type="text/css" href="design_1.css">
+        <link rel="stylesheet" type="text/css" href="design.css">
     </head>
     <body>
 
-
-
-<?php
-
-require_once 'connection.php';
-
-
-$search= filter_input(INPUT_POST, 'search');
-
-
-$sql="SELECT * FROM CELLPHONES WHERE BRAND LIKE '%$search%' OR MODELL LIKE '%$search%' OR CODE LIKE '%$search%'";
-
-$result=$base->prepare($sql);
-
-$result->execute();
-
-$rows_qty=$result->rowCount();
-
-  if($rows_qty<1){
-        echo 'Product not found'; 
-    
-    } else {
-
-while($register=$result->fetch(PDO::FETCH_ASSOC)){
+        <?php
+        session_start();
         
-    echo '<table>';
-        echo '<tr><td>Brand: '. $register['brand'].'</td></tr>';
-        echo '<tr><td>Modell: '.$register['modell'].'</td></tr>';
-        echo '</table';
-    }
-}
-
-?>
-                </td></tr></table>
+        if (isset($_SESSION['us'])) {
+        echo 'User ' . $_SESSION['us'];
+        }
         
+        ?>
+
         <p>
-<p>
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-    </p>    
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+        </p>    
+    <center>
+        <table width="25%">
+
+                   <tr>
+                   
+                       <td class="first_line">Brand</td>
+                       <td class="first_line">Modell</td>
+                   </tr>
+                    
+        <?php
+       
+        require_once 'connection.php';
+
+//Search query
+        
+        try{
+            
+        
+        $search = filter_input(INPUT_POST, 'search');
+
+        $result = $base->query("SELECT * FROM CELLPHONES WHERE BRAND LIKE '%$search%' OR MODELL LIKE '%$search%' OR CODE LIKE '%$search%'")->fetchAll(PDO::FETCH_OBJ);
+
+        //$result = $base->prepare($sql);
+
+      //  $result->execute();
+
+      //  $obs_qty=count((array)$result);
+
+        //$count = count((array) $data);
+        
+//        if($obs_qty>1){
+//       
+//
+//        } else {
+
+            
+            foreach($result as $res): 
+                ?>
+
+        
+            <tr><td><?php echo $res->brand; ?></td>
+            <td><?php echo $res->modell; ?></td></tr>
+        </table>
+</center>
+        <?php    endforeach;
+        //}
+        
+        } catch (Exception $ex) {
+
+             echo 'Product not found<br>';
+             
+             echo $ex->getMessage();
+             echo $ex->getLine();
+             
+             
+        }
+        ?>
+
+
         <p>
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-    </p>    
-            <p>
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-        &nbsp;
-    </p>    
-        </p>
-        <div style="justify-content: space-between; margin: 30px auto;">
+        <p>
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+        </p>    
+        <p>
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+        </p>    
+        <p>
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+        </p>    
+        <p>
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp; 
+        </p>    
+
+
+        <div style="justify-content: space-between; margin: 30px auto">
 
             <a href="createProduct.php"><input type="button" name="ins" value=" Create New Product" class="button"></a>
-</div>
+        </div>
         <p></p>   
         <p>
-    <div style="justify-content: space-between; margin: 30px auto;">
-    <a href="usersArea.php"><input type="button" class="button" name="back" value="Back"></a>
-    </div>
-           <div style="text-align: right;width:1020px; ">
-          <a href="closeSession.php"><input type="button" class= "button" name="btn" value="Close Session" </a>
+        <div style="justify-content: space-between; margin: 30px auto;">
+            <a href="usersArea.php"><input type="button" class="button" name="back" value="Back"></a>
         </div>
+        <div style="text-align: right;width:1080px; ">
+            <a href="closeSession.php"><input type="button" class= "button" name="btn" value="Close Session" </a>
+        </div>
+  
+    
     </body>
 </html>
